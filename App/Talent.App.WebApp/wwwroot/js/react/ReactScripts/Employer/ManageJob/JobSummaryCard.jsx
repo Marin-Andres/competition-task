4789615c6bfa7755c7520718bcf20411dd5f46b4
Andres Marin <andres.marin@gmx.com>
2024-10-30 16:16:49 +1100

    Connected job results from database to cards

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
@@ -15,14 +15,22 @@ export class JobSummaryCard extends React.Component {
     }
 
     render() {
+        const { job } = this.props;
+        //console.log("job:", job);
+        const title = job?.title;
+        const locationCity = job?.location?.city;
+        const locationCountry = job?.location?.country;
+        const jobSummary = job?.summary;
+        const noOfSuggestions = (job?.noOfSuggestions > 0) ? job.noOfSuggestions : 0;
+
         return (
             <Card style={{ width: '350px', padding: '10px', height: '400px' }}>
                 <Card.Content>
-                <Card.Header>Test Analyst Master</Card.Header>
-                <Label as='a' color='black' ribbon='right' ><Icon name='user' />0</Label>
-                <Card.Meta>Auckland, New Zealand</Card.Meta>
+                <Card.Header>{title}</Card.Header>
+                <Label as='a' color='black' ribbon='right' ><Icon name='user' />{noOfSuggestions}</Label>
+                <Card.Meta>{locationCity}, {locationCountry}</Card.Meta>
                 <Card.Description>
-                    We have a position for a Test Analyst.
+                    {jobSummary}
                 </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
@@ -37,44 +45,3 @@ export class JobSummaryCard extends React.Component {
         )
     }
 }
-// const sampleJobDoc = 
-// {
-//     "_id" : ObjectId("6715b730951b0b9745f6b2cd"),
-//     "EmployerID" : "670dd32b7652cc0758907f2f",
-//     "Title" : "Master debugger",
-//     "Description" : "<p>Constantly debugging</p>\n",
-//     "LogoUrl" : null,
-//     "Summary" : "Clear debugging",
-//     "CreatedOn" : ISODate("2024-10-21T02:06:40.287Z"),
-//     "ExpiryDate" : ISODate("2024-11-17T02:04:53.000Z"),
-//     "ApplicantDetails" : {
-//         "YearsOfExperience" : {
-//             "Years" : 1,
-//             "Months" : 1
-//         },
-//         "Qualifications" : [],
-//         "VisaStatus" : []
-//     },
-//     "JobDetails" : {
-//         "Categories" : {
-//             "Category" : "Engineering",
-//             "SubCategory" : "Management"
-//         },
-//         "JobType" : [ 
-//             "partTime"
-//         ],
-//         "StartDate" : ISODate("2024-10-22T02:04:57.000Z"),
-//         "EndDate" : ISODate("2025-01-14T02:04:45.000Z"),
-//         "Salary" : {
-//             "From" : 60000,
-//             "To" : 83000
-//         },
-//         "Location" : {
-//             "Country" : "Australia",
-//             "City" : "Melbourne"
-//         }
-//     },
-//     "Status" : 0,
-//     "TalentSuggestions" : null,
-//     "IsDeleted" : false
-// };
